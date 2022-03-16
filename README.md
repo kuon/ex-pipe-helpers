c077fbc3d5ed3a7eb433c51b9eb69d52a0cce6a3
Nicolas Goy <kuon@goyman.com>
2022-03-16 01:37:10 +0100

    Base library

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PipeHelpers
 
-**TODO: Add description**
+Pipe helpers are a set of simple helpers for elixir to help structure code with
+pipes.
 
 ## Installation
 
@@ -18,4 +19,26 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/pipe_helpers>.
+
+
+## Documentation
+
+
+The library is extensively documentd in the main module documentation.
+
+Small example:
+
+```elixir
+  import PipeHelpers
+
+  # Example in a live view
+  @impl true
+  def mount(_params, _session, socket) do
+    socket
+    |> assign(loading: true)
+    |> ok()
+  end
+
+```
+
 
--- lib/pipe_helpers.ex
+++ lib/pipe_helpers.ex
@@ -1,18 +1,95 @@
 defmodule PipeHelpers do
   @moduledoc """
-  Documentation for `PipeHelpers`.
+  Helper for piping data
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PipeHelpers.hello()
-      :world
-
+  Wrap into ok tuple
   """
-  def hello do
-    :world
+  def ok(val) do
+    {:ok, val}
   end
+
+  @doc """
+  Wrap into noreply tuple (genserver and phoenix socket format)
+  """
+  def noreply(val) do
+    {:noreply, val}
+  end
+
+  @doc """
+  Wrap into reply tuple (genserver and phoenix socket format)
+  """
+  def noreply(state, reply) do
+    {:reply, reply, state}
+  end
+
+  @doc """
+  Wrap into tuple pair
+  """
+  def pair(val, res) do
+    {res, val}
+  end
+
+  @doc """
+  Unwrap from a tuple pair
+  """
+  def unpair({_val, res}) do
+    res
+  end
+
+  @doc """
+  Wrap into tuple rpair
+  """
+  def rpair(val, res) do
+    {val, res}
+  end
+
+  @doc """
+  Tap only if ok tuple
+  """
+  def tap_ok(result, fun) do
+    then_ok(result, fun)
+    result
+  end
+
+  @doc """
+  Tap only if value match
+  """
+  def tap_on(result, value, fun) do
+    then_on(result, value, fun)
+    value
+  end
+
+  @doc """
+  Then only if ok tuple
+  """
+  def then_ok({:ok, ok_val} = _result, fun) do
+    fun
+    |> Function.info()
+    |> Keyword.get(:arity)
+    |> case do
+      0 -> fun.()
+      1 -> fun.(ok_val)
+      _ -> raise "then_ok function arity can only be 0 or 1"
+    end
+  end
+
+  def then_ok(result, _fun), do: result
+
+  @doc """
+  Then only if value match
+  """
+  def then_on(value, value, fun) do
+    fun
+    |> Function.info()
+    |> Keyword.get(:arity)
+    |> case do
+      0 -> fun.()
+      1 -> fun.(value)
+      _ -> raise "then_on function arity can only be 0 or 1"
+    end
+  end
+
+  def then_on(result, _value, _fun), do: result
 end
